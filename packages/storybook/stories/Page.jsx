5e08a4d4a002b6dfed981533b59c7dd6e73fe133
Ayanrinde damilola david <dammyayans560@gmail.com>
2023-06-20 17:19:43 +0100

    feat: Changed storybook to vite config

--- packages/storybook/stories/Page.jsx
+++ packages/storybook/stories/Page.jsx
@@ -3,12 +3,8 @@ import React from 'react';
 import { Header } from './Header';
 import './page.css';
 
-type User = {
-  name: string;
-};
-
-export const Page: React.FC = () => {
-  const [user, setUser] = React.useState<User>();
+export const Page = () => {
+  const [user, setUser] = React.useState();
 
   return (
     <article>
@@ -45,7 +41,7 @@ export const Page: React.FC = () => {
         </ul>
         <p>
           Get a guided tutorial on component-driven development at{' '}
-          <a href="renderers/react/template/cli/ts/Page" target="_blank" rel="noopener noreferrer">
+          <a href="https://storybook.js.org/tutorials/" target="_blank" rel="noopener noreferrer">
             Storybook tutorials
           </a>
           . Read more in the{' '}
